5ababe74a92700b6587c25adbc3da0af5b8698d4
Anyi Li <Anyi@AnyideMacBook-Air.local>
2017-04-23 21:28:52 +0800

    Initial Commit

---
+++ AnyiNetwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnyiNetwork
+//
+//  Created by Anyi Li on 2017/4/23.
+//  Copyright © 2017年 Anyi Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnyiNetwork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnyiNetwork
+//
+//  Created by Anyi Li on 2017/4/23.
+//  Copyright © 2017年 Anyi Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
